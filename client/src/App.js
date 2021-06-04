268d52db11b8cdb52d6fc9ab6c12bacbe47044b8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-04 09:21:32 -0500

    add todos component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import DisplayMessage from './components/DisplayMessage'
+import Todos from './components/Todos'
 
 export default function App() {
     return (
         <div>
             <DisplayMessage />
+            <Todos />
         </div>
     )
 }
---
+++ client/src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react'
+
+export default function Todos() {
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setTodos(data)
+      })
+    }, [])
+    return (
+        <div>
+        {todos.map((todo) => {
+            return (
+            <div key={todo.id}>
+                <h1>Todo Title: {todo.title}</h1>
+            </div>
+            )
+        })}
+        </div>
+    )
+}
